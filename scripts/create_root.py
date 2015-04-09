75c3a81a34fac6ed62854a8c7f4643bc4f21807a
Ma Ming <khahux@163.com>
2015-04-09 21:43:11 +0800

    init

--- scripts/create_root.py
+++ scripts/create_root.py
@@ -2,8 +2,8 @@
 
 __author__ = 'wangyu'
 
-from ..model.user import User
-from ..application import app
+from blog.model.user import User
+from blog.application import app
 
 
 def root():
--- scripts/create_table.py
+++ scripts/create_table.py
@@ -2,8 +2,8 @@
 
 __author__ = 'wangyu'
 
-from ..model.base import Base
-from ..application import app
+from blog.model.base import Base
+from blog.application import app
 
 
 Base.metadata.create_all(app.sa_engine)
